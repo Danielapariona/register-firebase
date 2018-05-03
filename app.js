aa45c9539180a6e3d2d69ec80a7734159c6a2fde
Danielapariona <daniela.parionac@gmail.com>
2018-05-03 16:49:24 -0500

    Save user data

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
+var config = {
+  apiKey: "AIzaSyCJJjruki7O6i_RwHk5TjvMJwdd430TSTo",
+  authDomain: "leads-interfondos.firebaseapp.com",
+  databaseURL: "https://leads-interfondos.firebaseio.com",
+  projectId: "leads-interfondos",
+  storageBucket: "leads-interfondos.appspot.com",
+  messagingSenderId: "411090660679"
+};
+firebase.initializeApp(config);
+
+/* Inputs */
+function writeUserData(name, email, dni) {
+  firebase.database().ref('leads-interfondos').push({
+    name: name,
+    email: email,
+    dni: dni
+  });
+}
+
 (function () {
   'use strict';
   window.addEventListener('load', function () {
     var forms = document.getElementsByClassName('needs-validation');
     var validation = Array.prototype.filter.call(forms, function (form) {
       form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        // datos se guardan
+        const name = document.getElementById('validationCustom01').value;
+        const email = document.getElementById('validationCustom02').value;
+        const dni = document.getElementById('validationCustom03').value;
+        writeUserData(name, email, dni);
+
         if (form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
