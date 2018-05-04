688c9a8adb7ea75893bc2d0f623500d6d11a9ece
Danielapariona <daniela.parionac@gmail.com>
2018-05-04 09:41:11 -0500

    Defines the number of DNI characters

--- app.js
+++ app.js
@@ -20,21 +20,27 @@ function writeUserData(name, email, dni) {
 (function () {
   'use strict';
   window.addEventListener('load', function () {
-    var forms = document.getElementsByClassName('needs-validation');
+    const forms = document.getElementsByClassName('needs-validation');
+    const dni = document.getElementById('validationCustom03');
+    dni.addEventListener('input', function () {
+      const emailValid = document.getElementById('invalid-dni');
+      (dni.value.length > 8) ? emailValid.classList.add('d-block') : emailValid.classList.remove('d-block');
+    });
+
     var validation = Array.prototype.filter.call(forms, function (form) {
       form.addEventListener('submit', function (event) {
         event.preventDefault();
         // Guarda datos de usuarios.
         const name = document.getElementById('validationCustom01').value;
         const email = document.getElementById('validationCustom02').value;
-        const dni = document.getElementById('validationCustom03').value;
-        if (name !== "" && email !== "" && dni !== "" && form.checkValidity()) {
-          writeUserData(name, email, dni);
+        const dni = document.getElementById('validationCustom03');
+        if (name !== "" && email !== "" && dni.value !== "" && form.checkValidity()) {
+          writeUserData(name, email, dni.value);
           document.getElementById('register-js').classList.add('d-none');
           document.getElementById('message').classList.remove('d-none');
         }
 
-        if (form.checkValidity() === false ) {
+        if (form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
         }
